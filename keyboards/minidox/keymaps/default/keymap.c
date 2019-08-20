7573d868d1fb4779974378b14500904569fd2958
ali <github@alihong.net>
2019-08-20 13:13:58 -0500

    wip

--- keyboards/minidox/keymaps/default/keymap.c
+++ keyboards/minidox/keymaps/default/keymap.c
@@ -40,10 +40,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                `------'    `------'
  */
 [_QWERTY] = LAYOUT( \
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-                    KC_LCTL, LOWER, KC_SPC,         KC_BSPC, RAISE, OSM(MOD_LSFT)                 \
+  KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,             \
+  KC_A,         KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,          \
+  LALT_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  LALT_T(KC_SLASH), \
+          OSM(MOD_LSFT), LGUI_T(KC_BSPACE), MO(1)       LT(2,KC_ENTER), KC_SPACE, KC_RCTRL                     \
 ),
 
 /* Raise
